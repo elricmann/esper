5c0b0d36e65ae09d42c1613fb868f9344be023dd
elricmann <>
2024-09-13 19:05:29 +0300

    feat: structopt, cc

--- src/cc.rs
+++ src/cc.rs
@@ -1,21 +1,53 @@
-use std::fs::File;
+use crate::emit::{EmitContextImpl, EmitDefault};
+use crate::parser::esper_parser;
+
+use std::fs::{self, File};
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
+
+pub(crate) fn compile(
+    input: PathBuf,
+    output: PathBuf,
+    clang_flags: Vec<String>,
+    use_prelude: bool,
+) {
+    let source = fs::read_to_string(input).expect("input file not found");
+
+    match esper_parser::program(&source) {
+        Ok(program) => {
+            // dbg!(&program);
+            let mut ctx = EmitContextImpl::new();
+            ctx.use_prelude = true;
+            let mut out = EmitDefault { ctx };
+            let out = out.emit_program(&program, "fib".into());
+            // println!("{}", &out);
+            ClangCXX::compile(&out, output.to_str().unwrap(), clang_flags).unwrap();
+        }
+
+        Err(err) => {
+            println!("{}", err);
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct ClangCXX;
 
 impl ClangCXX {
     // by default, we will avoid writing the cxx file unless an emit flag is used
-    pub fn compile(source: &str, output_file: &str) -> std::io::Result<()> {
+    pub fn compile(
+        cxx_source: &str,
+        output_file: &str,
+        clang_flags: Vec<String>,
+    ) -> std::io::Result<()> {
         let mut clang = Command::new("clang++")
             .arg("-x")
             .arg("c++")
             .arg("-") // read from stdin
             .arg("-o")
             .arg(output_file)
-            .args(["--std","c++20"]) // @todo: pass from argparse
+            .args(clang_flags)
             .stdin(Stdio::piped())
             .stdout(Stdio::inherit()) // forward stdout
             .stderr(Stdio::inherit()) // forward stderr
@@ -23,7 +55,7 @@ impl ClangCXX {
 
         // pipe the source to the clang++ process stdin
         if let Some(ref mut stdin) = clang.stdin {
-            stdin.write_all(source.as_bytes())?;
+            stdin.write_all(cxx_source.as_bytes())?;
         }
 
         clang.wait()?;
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
 #![allow(warnings, dead_code)]
+use std::path::PathBuf;
+use std::process::exit;
+use structopt::StructOpt;
 
 mod cc;
 mod emit;
 mod parser;
 mod visit;
 
-use emit::EmitContextImpl;
-
-use crate::cc::ClangCXX;
-use crate::emit::EmitDefault;
+use crate::cc::*;
 use crate::parser::esper_parser;
 
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+
+    #[structopt(short, long, parse(from_os_str))]
+    output: PathBuf,
+
+    #[structopt(short, long)]
+    emit: bool,
+
+    #[structopt(short = "cc", long = "clang-flags", use_delimiter = true)]
+    clang_flags: Vec<String>,
+}
+
 fn main() {
-    let source = include_str!("../tests/fib.esp");
-
-    match esper_parser::program(source) {
-        Ok(program) => {
-            // dbg!(&program);
-            let mut ctx = EmitContextImpl::new();
-            ctx.use_prelude = true;
-            let mut out = EmitDefault { ctx };
-            let out = out.emit_program(&program, "fib".into());
-            // println!("{}", &out);
-            ClangCXX::compile(&out, "./tests/fib").unwrap();
-        }
-
-        Err(err) => {
-            println!("{}", err);
-        }
-    }
+    let args = Opt::from_args();
+    compile(args.input, args.output, args.clang_flags, args.emit);
 }
